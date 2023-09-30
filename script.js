efee4eeab99f698c09d0a649255e621e670682d8
Md Fahimul Kabir Chowdhury <66033826+fahimulkabir@users.noreply.github.com>
2023-09-30 23:47:56 +0600

    creating responsive navbar

--- script.js
+++ script.js
@@ -1,14 +1,36 @@
-let hamMenuIcon = document.getElementById("ham-menu");
-let navBar = document.getElementById("nav-bar");
-let navLinks = navBar.querySelectorAll("li");
+// const bar = document.getElementById("bar");
+// const nav = document.getElementById("navbar");
 
-hamMenuIcon.addEventListener("click", () => {
-  navBar.classList.toggle("active");
-  hamMenuIcon.classList.toggle("fa-times");
-});
-navLinks.forEach((navLinks) => {
-  navLinks.addEventListener("click", () => {
-    navBar.classList.remove("active");
-    hamMenuIcon.classList.toggle("fa-times");
+// if (bar) {
+//   bar.addEventListener("click", () => {
+//     if (nav.classList.contains("active")) {
+//       nav.classList.remove("active");
+//     } else {
+//       nav.classList.add("active");
+//     }
+//   });
+// }
+
+const bar = document.getElementById("bar");
+const nav = document.getElementById("navbar");
+
+// Function to handle clicks outside the nav list
+function handleClickOutside(event) {
+  if (!nav.contains(event.target) && !bar.contains(event.target)) {
+    nav.classList.remove("active");
+    // Remove the event listener to prevent further clicks from closing the menu
+    document.body.removeEventListener("click", handleClickOutside);
+  }
+}
+
+if (bar) {
+  bar.addEventListener("click", () => {
+    if (nav.classList.contains("active")) {
+      nav.classList.remove("active");
+    } else {
+      nav.classList.add("active");
+      // Add the event listener to detect clicks outside the nav list
+      document.body.addEventListener("click", handleClickOutside);
+    }
   });
-});
+}
