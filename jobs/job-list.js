6bd36a9817c3a700d1e7e5315cf578619b7ea810
Md Fahimul Kabir Chowdhury <66033826+fahimulkabir@users.noreply.github.com>
2023-09-30 23:40:22 +0600

    Creating folder structure

---
+++ jobs/job-list.js
@@ -0,0 +1,213 @@
+const jCategory = [
+  {
+    index: 0,
+    image: "../images/artemis.jpg",
+    title: "Artemis I",
+    by: "<a href='#' target='__blank__'>Jhon Doe</a>",
+    abastract:
+      "Artemis I, formerly Exploration Mission-1,will be the first integrated flight test of NASA’s Deep Space Exploration Systems: the Orion spacecraft, Space Launch System (SLS) rocket, with the newly upgraded Exploration Ground Systems at Kennedy Space Center in Cape Canaveral, Florida.",
+    description:
+      "Artemis I, formerly Exploration Mission-1,will be the first integrated flight test of NASA’s Deep Space Exploration Systems: the Orion spacecraft, Space Launch System (SLS) rocket, with the newly upgraded Exploration Ground Systems at Kennedy Space Center in Cape Canaveral, Florida. The primary operations goal of the mission is to assure a safe crew module entry, descent, splashdown, and recovery. In addition to sending Orion on its journey around the Moon, SLS will carry 10 small satellites that will perform their own science and technology investigations. The first in a series of increasingly complex missions, Artemis I will provide a foundation for human deep space exploration and demonstrate our commitment and capability to extend human existence to the Moon and beyond prior to the first flight with crew on Artemis II. Artemis I is foundational to the space economy, fueling new industries and technologies, supporting job growth, and furthering the demand for a highly skilled work force. Men and women in all fifty states are hard at work building the Deep Space Exploration Systems to support missions to deep space. NASA prime contractors, Aerojet Rocketdyne, Boeing, Jacobs, Lockheed Martin, and Northrop Grumman currently have over 3,200 suppliers contributing to the milestone achievement that heralds the success of America’s human spaceflight program.",
+    workplace: "Work from home",
+    role: "Technical Expertise, Mission Planning and Analysis, Technological Innovation",
+    requirements:
+      "<li>Aerospace engineering skills</li>" +
+      "<li>Expertise in space science</li>" +
+      "<li>Proficiency in propulsion systems</li>" +
+      "<li>Mission planning expertise</li>" +
+      "<li>New technology proposals</li>",
+    documentation:
+      "<a href='' target='__blank__'>Artemis I</a>" +
+      "<br>" +
+      "<a href='' target='__blank__'>Artemis I Map</a>",
+    timeline: "December 2023",
+  },
+  {
+    index: 1,
+    image: "../images/lucy.png",
+    title: "Lucy: The First Mission to the Trojan Asteroids",
+    by: "<a href='#' target='__blank__'>Jhon Doe</a>",
+    abastract:
+      "The Lucy spacecraft launched on October 16, 2021, and will make flybys of seven Trojan asteroids over the next ten years. The first flyby will be of the asteroid 522466 (2010 TK7), which is a near-Earth asteroid. The remaining six flybys will be of Trojan asteroids, including four that are members of binary systems",
+    description:
+      "The Lucy mission is named after the fossilized human ancestor (called “Lucy” by her discoverers) whose skeleton provided unique insight into humanity's evolution. Likewise, the Lucy mission will revolutionize our knowledge of planetary origins and the formation of the solar system.The Lucy spacecraft launched on October 16, 2021, and will make flybys of seven Trojan asteroids over the next ten years. The first flyby will be of the asteroid 522466 (2010 TK7), which is a near-Earth asteroid. The remaining six flybys will be of Trojan asteroids, including four that are members of binary systems." +
+      "<li>To study the composition and structure of the Trojan asteroids</li>" +
+      "<li>To determine the age and origin of the Trojan asteroids.</li>" +
+      "<li>To study the dynamics of the Trojan asteroids and their interactions with Jupiter.</li>" +
+      "<li>To search for organic molecules and other potential biosignatures on the Trojan asteroids.</li>",
+    workplace: "Work from home",
+    role: "Technical Expertise, Mission Planning and Analysis, Technological Innovation",
+    requirements:
+      "<li>Aerospace engineering skills</li>" +
+      "<li>Expertise in space science</li>" +
+      "<li>Proficiency in propulsion systems</li>" +
+      "<li>Mission planning expertise</li>" +
+      "<li>New technology proposals</li>",
+    documentation:
+      "<a href='' target='__blank__'>More Details</a>" +
+      "<br>" +
+      "<a href='' target='__blank__'>Images</a>",
+    timeline: "November 2033",
+  },
+  {
+    index: 2,
+    image: "../images/hubble.jpg",
+    title: "Hubble Space Telescope",
+    by: "<a href='#' target='__blank__'>Jhon Doe</a>",
+    abastract:
+      "Artemis I, formerly Exploration Mission-1,will be the first integrated flight test of NASA’s Deep Space Exploration Systems: the Orion spacecraft, Space Launch System (SLS) rocket, with the newly upgraded Exploration Ground Systems at Kennedy Space Center in Cape Canaveral, Florida.",
+    description:
+      "Named in honor of the trailblazing astronomer Edwin Hubble, the Hubble Space Telescope is a large, space-based observatory, which has revolutionized astronomy since its launch and deployment by the space shuttle Discovery in 1990. Far above rain clouds, light pollution, and atmospheric distortions, Hubble has a crystal-clear view of the universe. Scientists have used Hubble to observe some of the most distant stars and galaxies yet seen, as well as the planets in our solar system. Hubble’s capabilities have grown immensely in its over 30 years of operation. This is because new, cutting-edge scientific instruments have been added to the telescope over the course of five astronaut servicing missions. By replacing and upgrading aging parts, these servicing missions have greatly extended the telescope’s lifetime. Telescopes have a particular range of light that they can detect. Hubble’s domain extends from the ultraviolet through the visible (which our eyes see) and into the near-infrared. This range has allowed Hubble to deliver stunning images of stars, galaxies, and other astronomical objects that have inspired people around the world and changed our understanding of the universe.",
+    workplace: "Work from home",
+    role: "Technical Expertise, Mission Planning and Analysis, Technological Innovation",
+    requirements:
+      "<li>Aerospace engineering skills</li>" +
+      "<li>Expertise in space science</li>" +
+      "<li>Proficiency in propulsion systems</li>" +
+      "<li>Mission planning expertise</li>" +
+      "<li>New technology proposals</li>",
+    documentation:
+      "<a href='' target='__blank__'>Artemis I</a>" +
+      "<br>" +
+      "<a href='' target='__blank__'>Artemis I Map</a>",
+    timeline: "December 2023",
+  },
+  {
+    index: 3,
+    image: "../images/webb.png",
+    title: "James Webb Space Telescope",
+    by: "<a href='#' target='__blank__'>Jhon Doe</a>",
+    abastract:
+      "The James Webb space Telescope is the largest and most powerful telescope yet created in space. It uses infrared light to observe things that are insufficiently powerful, far away, or ancient for other telescopes to view.",
+    description:
+      "The James Webb Space Telescope (sometimes called JWST or Webb) will be a large infrared telescope with a 6.5-meter primary mirror. The telescope will be launched on an Ariane 5 rocket from French Guiana in 2021. The Webb telescope will be the premier observatory of the next decade, serving thousands of astronomers worldwide. It will study every phase in the history of our Universe, ranging from the first luminous glows after the Big Bang, to the formation of solar systems capable of supporting life on planets like Earth, to the evolution of our own Solar System. The Webb telescope was formerly known as the Next Generation Space Telescope (NGST); it was renamed in September 2002 after a former NASA administrator, James Webb. Webb is an international collaboration between NASA, ESA (the European Space Agency), and the Canadian Space Agency (CSA). NASA's Goddard Space Flight Center in Greenbelt, Maryland, is managing the development effort. The main industrial partner is Northrop Grumman; the Space Telescope Science Institute will operate Webb after launch.",
+    workplace: "Work from home",
+    role: "Project Scientist, Instrument Scientist, Systems Engineer, Launch Engineer, Operations Engineer, Astronomer, Engineer, Technician, Administrator, Data Scientist ,Software Engineer, Education and Public Outreach Specialist,Safety Officer,Logistics Specialist",
+    requirements:
+      "<li>Aerospace engineering skills</li>" +
+      "<li>Expertise in space science</li>" +
+      "<li>Proficiency in propulsion systems</li>" +
+      "<li>Mission planning expertise</li>" +
+      "<li>New technology proposals</li>",
+    documentation:
+      "<a href='' target='__blank__'>James Webb</a>" +
+      "<br>" +
+      "<a href='' target='__blank__'>Video</a>",
+    timeline: "December 2033",
+  },
+  {
+    index: 4,
+    image: "../images/juno.jpg",
+    title: "Juno: Mission at Jupiter",
+    by: "<a href='#' target='__blank__'>Jhon Doe</a>",
+    abastract:
+      "Artemis I, formerly Exploration Mission-1,will be the first integrated flight test of NASA’s Deep Space Exploration Systems: the Orion spacecraft, Space Launch System (SLS) rocket, with the newly upgraded Exploration Ground Systems at Kennedy Space Center in Cape Canaveral, Florida.",
+    description:
+      "On August 5, 2011, NASAs Juno spacecraft embarked on a 5-year journey to our solar system's largest planet the gas giant Jupiter. Its mission: to probe beneath the planet's dense clouds and answer questions about the origin and evolution of Jupiter, our solar system, and giant planets in general across the cosmos. Juno arrived at Jupiter on July 4, 2016, after a 5-year, 1.7-billion-mile journey, and settled into a 53-day polar orbit stretching from just above Jupiters cloud tops to the outer reaches of the Jovian magnetosphere. Junos discoveries have revolutionized our understanding of Jupiter and solar system formation. During the prime missions 35 orbits of Jupiter, Juno collected more than three terabits (375 gigabytes) of science data and provided dazzling views of Jupiter and its satellites, all processed by citizen scientists with NASAs first-ever camera dedicated to public outreach. Junos many discoveries have changed our view of Jupiters atmosphere and interior, revealing an atmospheric weather layer that extends far beyond its clouds and a deep interior with a diluted, or fuzzy, heavy element core. Near the end of the prime mission, as the spacecrafts orbit evolved, flybys of the moon Ganymede initiated Junos transition into a full Jovian system explorer.",
+    workplace: "Work from home",
+    role: "Technical Expertise, Mission Planning and Analysis, Technological Innovation",
+    requirements:
+      "<li>Aerospace engineering skills</li>" +
+      "<li>Expertise in space science</li>" +
+      "<li>Proficiency in propulsion systems</li>" +
+      "<li>Mission planning expertise</li>" +
+      "<li>New technology proposals</li>",
+    documentation:
+      "<a href='' target='__blank__'>Artemis I</a>" +
+      "<br>" +
+      "<a href='' target='__blank__'>Artemis I Map</a>",
+    timeline: "December 2023",
+  },
+  {
+    index: 5,
+    image: "../images/station.jpg",
+    title: "International Space Station",
+    by: "<a href='#' target='__blank__'>Jhon Doe</a>",
+    abastract:
+      "Artemis I, formerly Exploration Mission-1,will be the first integrated flight test of NASA’s Deep Space Exploration Systems: the Orion spacecraft, Space Launch System (SLS) rocket, with the newly upgraded Exploration Ground Systems at Kennedy Space Center in Cape Canaveral, Florida.",
+    description:
+      "The Mars Perseverance rover mission is part of NASA's Mars Exploration Program, a long-term effort of robotic exploration of the Red Planet. The Mars Perseverance mission addresses high-priority science goals for Mars exploration, including key questions about the potential for life on Mars. The mission takes the next step by not only seeking signs of habitable conditions on Mars in the ancient past, but also searching for signs of past microbial life itself. The Mars Perseverance rover introduces a drill that can collect core samples of the most promising rocks and soils and set them aside in a cache on the surface of Mars. The mission also provides opportunities to gather knowledge and demonstrate technologies that address the challenges of future human expeditions to Mars. These include testing a method for producing oxygen from the Martian atmosphere, identifying other resources (such as subsurface water), improving landing techniques, and characterizing weather, dust, and other potential environmental conditions that could affect future astronauts living and working on Mars.",
+    workplace: "Work from home",
+    role: "Technical Expertise, Mission Planning and Analysis, Technological Innovation",
+    requirements:
+      "<li>Aerospace engineering skills</li>" +
+      "<li>Expertise in space science</li>" +
+      "<li>Proficiency in propulsion systems</li>" +
+      "<li>Mission planning expertise</li>" +
+      "<li>New technology proposals</li>",
+    documentation:
+      "<a href='' target='__blank__'>Artemis I</a>" +
+      "<br>" +
+      "<a href='' target='__blank__'>Artemis I Map</a>",
+    timeline: "December 2023",
+  },
+  {
+    index: 6,
+    image: "../images/rover.jpg",
+    title: "Perseverance Mars Rover",
+    by: "<a href='#' target='__blank__'>Jhon Doe</a>",
+    abastract:
+      "Artemis I, formerly Exploration Mission-1,will be the first integrated flight test of NASA’s Deep Space Exploration Systems: the Orion spacecraft, Space Launch System (SLS) rocket, with the newly upgraded Exploration Ground Systems at Kennedy Space Center in Cape Canaveral, Florida.",
+    description:
+      "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
+    workplace: "Work from home",
+    role: "Technical Expertise, Mission Planning and Analysis, Technological Innovation",
+    requirements:
+      "<li>Aerospace engineering skills</li>" +
+      "<li>Expertise in space science</li>" +
+      "<li>Proficiency in propulsion systems</li>" +
+      "<li>Mission planning expertise</li>" +
+      "<li>New technology proposals</li>",
+    documentation:
+      "<a href='' target='__blank__'>Artemis I</a>" +
+      "<br>" +
+      "<a href='' target='__blank__'>Artemis I Map</a>",
+    timeline: "December 2023",
+  },
+  {
+    index: 7,
+    image: "../images/parker.jpg",
+    title: "Parker Solar Probe",
+    by: "<a href='#' target='__blank__'>Jhon Doe</a>",
+    abastract:
+      "Artemis I, formerly Exploration Mission-1,will be the first integrated flight test of NASA’s Deep Space Exploration Systems: the Orion spacecraft, Space Launch System (SLS) rocket, with the newly upgraded Exploration Ground Systems at Kennedy Space Center in Cape Canaveral, Florida.",
+    description:
+      "In order to unlock the mysteries of the Sun's atmosphere, Parker Solar Probe uses Venus’ gravity during seven flybys over nearly seven years to gradually bring its orbit closer to the Sun. The spacecraft will fly through the Sun’s atmosphere as close as 3.8 million miles to our star’s surface, well within the orbit of Mercury and more than seven times closer than any spacecraft has come before. (Earth’s average distance to the Sun is 93 million miles.) Flying into the outermost part of the Sun's atmosphere, known as the corona, for the first time, Parker Solar Probe employs a combination of in situ measurements and imaging to revolutionize our understanding of the corona and expand our knowledge of the origin and evolution of the solar wind. It also makes critical contributions to our ability to forecast changes in Earth's space environment that affect life and technology on Earth.",
+    workplace: "Work from home",
+    role: "Technical Expertise, Mission Planning and Analysis, Technological Innovation",
+    requirements:
+      "<li>Aerospace engineering skills</li>" +
+      "<li>Expertise in space science</li>" +
+      "<li>Proficiency in propulsion systems</li>" +
+      "<li>Mission planning expertise</li>" +
+      "<li>New technology proposals</li>",
+    documentation:
+      "<a href='' target='__blank__'>Artemis I</a>" +
+      "<br>" +
+      "<a href='' target='__blank__'>Artemis I Map</a>",
+    timeline: "December 2023",
+  },
+  {
+    index: 8,
+    image: "../images/dart.jpg",
+    title: "Double Asteroid Redirection Test (DART)",
+    by: "<a href='#' target='__blank__'>Jhon Doe</a>",
+    abastract:
+      "An on-orbit demonstration of asteroid deflection is a key test that NASA and other agencies wish to perform before any actual need is present. The DART mission is NASA's demonstration of kinetic impactor technology, impacting an asteroid to adjust its speed and path. DART is the first-ever space mission to demonstrate asteroid deflection by kinetic impactor. The spacecraft launched on a SpaceX Falcon 9 rocket out of Vandenberg Space Force Base in California.",
+    description:
+      "NASA launched a spacecraft in November 2021 called the Double Asteroid Redirection Test (DART) mission. The aim of the mission is to evaluate the kinetic impactor asteroid deflection technique. In order to alter a spacecraft's trajectory, this approach entails crashing it with an asteroid. DART is aiming for the binary asteroid Didymos, which is made up of the bigger Didymos and the smaller Dimorphos. Dimorphos is orbiting Didymos, and it will crash with the DART satellite. Dimorphos' speed is anticipated to be altered by the impact by a little percentage. Dimorphos's orbit around Didymos will be significantly impacted by this tiny speed shift, which will also further scientific knowledge on the use of kinetic impact to divert asteroids. The DART mission represents a significant advance in planetary protection. It is the first time that a spacecraft has been directed to strike an asteroid on purpose. The accomplishment of the mission will open the door for future missions that could divert asteroid threats to Earth.",
+    workplace: "Work from home",
+    role: "Technical Expertise, Mission Planning and Analysis, Technological Innovation",
+    requirements:
+      "<li>Aerospace engineering skills</li>" +
+      "<li>Expertise in space science</li>" +
+      "<li>Proficiency in propulsion systems</li>" +
+      "<li>Mission planning expertise</li>" +
+      "<li>New technology proposals</li>",
+    documentation:
+      "<a href='' target='__blank__'>More info</a>" +
+      "<br>" +
+      "<a href='' target='__blank__'>Video</a>",
+    timeline: "September 2022",
+  },
+];
