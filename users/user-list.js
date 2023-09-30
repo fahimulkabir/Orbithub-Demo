c98d6353f1ca1cfc797cae272907813a048fcb81
Md Fahimul Kabir Chowdhury <66033826+fahimulkabir@users.noreply.github.com>
2023-09-30 23:51:34 +0600

    adding new user, user list, user profile

---
+++ users/user-list.js
@@ -0,0 +1,21 @@
+const jCategory = [
+  {
+    index: 0,
+    userImage: "../images/artemis.jpg",
+    userName: "Jhon Doe",
+    heroPosition: "Computer Graphic Designer",
+    by: "<a href='#' target='__blank__'>Jhon Doe</a>",
+    shortBio:
+      "Artemis I, formerly Exploration Mission-1,will be the first integrated flight test of NASA’s Deep Space Exploration Systems",
+    breifBio:
+      "Artemis I, formerly Exploration Mission-1,will be the first integrated flight test of NASA’s Deep Space Exploration Systems: the Orion spacecraft, Space Launch System (SLS) rocket, with the newly upgraded Exploration Ground Systems at Kennedy Space Center in Cape Canaveral, Florida. The primary operations goal of the mission is to assure a safe crew module entry, descent, splashdown.",
+    positionOne:
+      "<span>2020 - Present</span>" +
+      "<li>Aerospace engineering skills</li>" +
+      "<li>Expertise in space science</li>" +
+      "<li>Proficiency in propulsion systems</li>",
+    objective:
+      "<ul><li><i class='bx bxs-location-plus'></i> It is going to be organized from October 6 and October 7, 2023, simultaneously and virtually all over the world.</li></ul>",
+    timeline: "December 2023",
+  },
+];
