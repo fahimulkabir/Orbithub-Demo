060298f7ad399c19b3fe5ac2faa551efc058f521
Md Fahimul Kabir Chowdhury <66033826+fahimulkabir@users.noreply.github.com>
2023-10-01 14:02:09 +0600

    forms creation

--- users/usersearch.js
+++ users/usersearch.js
@@ -7,7 +7,7 @@ jCategory.forEach((item) => {
 document.getElementById("searchBar").addEventListener("keyup", (e) => {
   const searchData = e.target.value.toLowerCase();
   const filteredData = categories.filter((item) => {
-    return item.title.toLowerCase().includes(searchData);
+    return item.userName.toLowerCase().includes(searchData);
   });
   displayItem(filteredData);
 });
@@ -15,21 +15,22 @@ document.getElementById("searchBar").addEventListener("keyup", (e) => {
 const displayItem = (items) => {
   document.getElementById("root").innerHTML = "";
   items.forEach((item) => {
-    var { index, image, title, abastract } = item;
+    var { index, userImage, userName, heroPosition, contro } = item;
     const jList = document.createElement("div");
-    jList.className = "jList";
+    jList.className = "userList";
     jList.innerHTML = `
-      <img src=${image} alt="">
+      <img src=${userImage} alt="">
       <div>
-      <h3>${title}</h3>
-      <p>${abastract}</p>
+      <h3>${userName}</h3>
+      <p>🎯 ${heroPosition}</p>
+      <h6>🚀 Contributed in ${contro} projects</h6>
       </div>
-      <span id="key">Project Details</span>
+      <span id="key">View Portfolio</span>
     `;
 
     // Add event listener for redirection
     jList.addEventListener("click", () => {
-      window.location.href = `job-details.html?id=${index}`;
+      window.location.href = `user-details.html?id=${index}`;
     });
 
     document.getElementById("root").appendChild(jList);
